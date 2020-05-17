1e8d9a9cc8b8ece3742e67c721d591b643b69a51
ludustyn121 <smdedar@aol.com>
2020-05-17 20:20:31 +0000

    load mode based on system mode

--- darkmode.js
+++ darkmode.js
@@ -2,3 +2,14 @@ function darkmode() {
    var element = document.body;
    element.classList.toggle("dark-mode");
 }
+
+ //Automatic Switch to Dark or Lite
+  function autoModeSwitch() {
+    var element = document.body;
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      // dark mode
+      element.classList.toggle("dark-mode");
+      document.getElementById("modeSwitch").checked = true;
+    }
+  }
+  autoModeSwitch();
